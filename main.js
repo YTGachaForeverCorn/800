d548da80c9ac9cfa56b5766c6d433ffed3fe2a24
YTGachaForeverCorn <79651055+YTGachaForeverCorn@users.noreply.github.com>
2021-03-16 17:59:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+name_of_the_student_array = [];
+
+	
+function submit()
+{
+    
+    var name_1 = document.getElementById("name_of_the_student_1").value;
+    var name_2 = document.getElementById("name_of_the_student_2").value;
+    var name_3 = document.getElementById("name_of_the_student_3").value;
+    var name_4 = document.getElementById("name_of_the_student_4").value;
+    var name_5 = document.getElementById("name_of_the_student_5").value;
+    var name_6 = document.getElementById("name_of_the_student_6").value;
+    var name_7 = document.getElementById("name_of_the_student_7").value;
+    var name_8 = document.getElementById("name_of_the_student_8").value
+    
+    name_of_the_student_array.push(name_1);
+    name_of_the_student_array.push(name_2);
+    name_of_the_student_array.push(name_3);
+    name_of_the_student_array.push(name_4);
+    name_of_the_student_array.push(name_5);
+    name_of_the_student_array.push(name_6);
+    name_of_the_student_array.push(name_7);
+    name_of_the_student_array.push(name_8);
+    console.log(name_of_the_student_array);
+    
+    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+    document.getElementById("display_name").innerHTML=name_of_student_array.join(". ");
+}
